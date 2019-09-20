102b82854c2bc3b2135937f863033020d54ee5d1
egoing <egoing@gmail.com>
2019-09-20 15:44:42 +0900

    5. 메소드의 출력

--- WhyMethod.java
+++ WhyMethod.java
@@ -1,22 +1,24 @@
+import java.io.FileWriter;
+import java.io.IOException;
 
 public class WhyMethod {
 	
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		
-			             //인자, argument
-			printTwoTimes("a", "-");
-			// 100000000
-			printTwoTimes("a", "*");
-			// 100000000
-			printTwoTimes("a", "&");
-			printTwoTimes("b", "!");
+			System.out.println(twoTimes("a", "-"));
+			FileWriter fw = new FileWriter("out.txt");
+			fw.write(twoTimes("a", "*"));
+			fw.close();
+//			Email.send("egoing@a.com", "two times a", twoTimes("a", "&"));
 
 	}
-								     //매개변수,parameter 
-	public static void printTwoTimes(String text, String delimiter) {
-		System.out.println(delimiter);
-		System.out.println(text);
-		System.out.println(text);
+	public static String twoTimes(String text, String delimiter) {
+		String out = "";
+		out = out + delimiter + "\n";
+		out = out + text + "\n";
+		out = out + text + "\n";
+		return out;
 	}
+					
 
 }
